804a3421cc82e11154b089bfd6b84f44ef35b51e
setoudie <setoudie@gmail.com>
2024-07-27 13:08:50 +0000

    first commit

---
+++ etl_banks_projects.py
@@ -0,0 +1,8 @@
+"""
+    Dans ce code nous allons creer un processus (ETL) pour
+    1. Extraire des donnees a l'aide de BeautifulSoup sur le Web
+    2. Transformer ces donnees en utilusant Pandas
+    3. Et enfin les sauvegarder dans une base de donnes
+"""
+# print("test")
+
